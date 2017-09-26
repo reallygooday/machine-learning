f91318f6e57b99793b677c77ab7b227eb878659a
Angela <reallygooday@users.noreply.github.com>
2017-09-26 22:35:27 +0100

    displaying state
    forcing the driving agent to use Q-learning

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -159,10 +159,10 @@ def run():
     # Create the driving agent
     # Flags:
     #   learning   - set to True to force the driving agent to use Q-learning
-    learning = False
+    learning = True
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, learning = True)
     
     ##############
     # Follow the driving agent
